4912f26766e43d27b48ac0cc06b9b5e6ea86f4ab
Austin <austinmaithya25@gmail.com>
2026-05-11 14:25:55 +0300

    New Changes

--- loan_risk_prediction_dataset.sql
+++ loan_risk_prediction_dataset.sql
@@ -2,3 +2,26 @@
 SELECT * FROM loan_risk_prediction_dataset;
 
 SHOW COLUMNS FROM loan_risk_prediction_dataset;
+-- Deleting Null Values 
+DELETE FROM
+ loan_risk_prediction_dataset
+    WHERE Age IS NULL
+    AND Income IS NULL
+    AND LoanAmount IS NULL 
+    AND CreditScore IS NULL
+    AND YearsExperience IS NULL
+    AND Gender IS NULL
+    AND Education IS NULL
+    AND City IS NULL
+    AND EmploymentType IS NULL
+    AND LoanApproved IS NULL;
+
+-- checking if the nulls are deleted 
+SHOW COLUMNS FROM loan_risk_prediction_dataset;
+
+
+SELECT * FROM loan_risk_prediction_dataset
+WHERE Income IS NULL;
+
+DELETE FROM loan_risk_prediction_dataset
+WHERE Income IS NULL OR Income = '';   
