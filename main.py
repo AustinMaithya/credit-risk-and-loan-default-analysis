47882c467a1e0d9ad50c472fe6cf96e63dd8eb21
Austin <austinmaithya25@gmail.com>
2026-05-19 16:46:16 +0300

    More changes

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from credit-risk-and-loan-default-analysis!")
+import joblib
+
+model = joblib.load(
+    "loan_approval_model.pkl"
+)
+
+print("Model loaded")
 
 
-if __name__ == "__main__":
-    main()
+
+new_customer = [[
+    35,      # Age
+    65000,   # Income
+    20000,   # LoanAmount
+    720,     # CreditScore
+    8,       # YearsExperience
+    1,       # Gender
+    2,       # Education
+    0,       # City
+    1        # EmploymentType
+]]
+
+prediction = model.predict(
+    new_customer
+)
+
+print(prediction)
+
+
